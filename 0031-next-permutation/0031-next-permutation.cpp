33358ece4827d909e15b710cb2ef91adbf05e6f7
JayeshChak <thewabisabiwaylearnjc@gmail.com>
2024-05-30 00:09:56 +0530

    Time: 4 ms (57.01%), Space: 14.6 MB (14.31%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -2,34 +2,34 @@ class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
         
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        cout.tie(NULL);
-        
-        int index = -1;
         int n = nums.size();
-
-        for(int i = n-2 ; i>=0 ;i--){
-            if(nums[i]<nums[i+1]){
+        // 2 1 5 4 3 0 0
+        // 0 1 2 3 4 5 6 
+        int index = -1;
+        
+        for(int i = n-2 ; i >=0 ; i--){
+            if(nums[i] < nums[i+1]){
                 index = i;
                 break;
             }
         }
-
+        
         if(index == -1){
-            reverse(nums.begin(), nums.end());
+            reverse(nums.begin(),nums.end());
             return;
         }
-
-        for(int i = n-1 ; i >= index ;i--){
-            if(nums[i]>nums[index]){
+        
+        int toReplace = nums[index];
+        
+        for(int i = n-1 ; i > index ; i--){
+            
+            if(nums[i] > nums[index]){
                 swap(nums[i],nums[index]);
                 break;
             }
         }
-
-        reverse(nums.begin() + index + 1 , nums.end());
-
-        return ;       
+        
+        reverse(nums.begin() + (index+1), nums.end());
+        
     }
 };
